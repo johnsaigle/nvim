9629a7e521720416eb6b7f9e7deac9dc97125b25
John Saigle <john@asymmetric.re>
2024-03-08 08:58:52 -0500

    Use nomicfoundation LSP for solidity support

--- init.lua
+++ init.lua
@@ -373,7 +373,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'solidity', 'tsx', 'typescript', 'vim' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
@@ -499,6 +499,7 @@ local servers = {
   gopls = {},
   pyright = {},
   rust_analyzer = {},
+  solidity_ls_nomicfoundation = {},
   tsserver = {},
 
   lua_ls = {
