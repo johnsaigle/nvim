07ec4495275711913582848121ef6fe4ad999fcb
John Saigle <john@asymmetric.re>
2024-10-24 17:39:00 -0400

    Do some prompt engineering

--- lua/custom/plugins/ollama.lua
+++ lua/custom/plugins/ollama.lua
@@ -45,7 +45,19 @@ return {
         input_label = "> ",
         model = "llama3.2",
         action = "display",
-      }
+      },
+      Summarize_Lint = {
+        prompt = "This text is the output of a code linter. Summarize the results into a general overview, grouped by issue type. Avoid chatter. Do not include any code in your response. Do not make suggestions or changes. Do not add code snippets. Here is the output of the linter: ```$sel```. ",
+        input_label = "> ",
+        model = "llama3.2",
+        action = "display",
+      },
+      Document_Block = {
+        prompt = "This $ftype code block needs better documentation. Summarize the code concisely at a high-level. Format your response in the correct commenting convention for the programming language. Make sure to document security-related functionality. Avoid chatter. Do not include the source code in your response. Do not summarize the work you have done. Group your comments together so that can be added to the top of the code block. Here is the code that needs commenting: $sel.",
+        input_label = "> ",
+        model = "llama3.2",
+        action = "display",
+      },
     }
     -- View the actual default prompts in ./lua/ollama/prompts.lua
     -- prompts = {
