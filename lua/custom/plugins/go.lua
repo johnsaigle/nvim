6b9413b8390d92c92e3ab4c453a9a9fc6358b3b3
John Saigle <john@asymmetric.re>
2024-10-25 10:53:46 -0400

    add macro to format byte strings

--- lua/custom/plugins/go.lua
+++ lua/custom/plugins/go.lua
@@ -1,6 +1,7 @@
--- Go 
-
-
 return {
-	vim.keymap.set('n', '<leader>ee', 'oif err != nil {<CR>}<Esc>Oreturn err<Esc>')
+	-- [[Go macros]]
+	-- do err == nil check and change contents within braces
+	vim.keymap.set('n', '<leader>ee', 'oif err != nil {<CR>}<Esc>Oreturn err<Esc>'),
+	-- Format a byte string
+	vim.keymap.set('n', '<leader>fb', 'ifmt.Sprintf("%x", byteStr)<Esc>Fb'),
 }
