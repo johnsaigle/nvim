bc62e1511af72826a3dda58e4721d64fc00f2adc
a <a>
2024-11-16 16:04:35 -0500

    fix avante to work with ollama

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -5,43 +5,103 @@ return {
   version = false, -- set this if you want to always pull the latest change
   opts = {
     provider = "ollama",
+    use_absolute_path = true,
     vendors = {
       ---@type AvanteProvider
       ollama = {
-        ["local"] = true,
-        endpoint = "http://localhost:11434/v1",
-        model = "llama3.2", -- TODO: FInd something more suited for this task
+        ['local'] = true,
+        endpoint = "http://nibbler:11434/v1",
+        -- endpoint = "http://localhost:11434/v1",
+        model = "qwen2.5-coder:7b-instruct-q8_0",
         parse_curl_args = function(opts, code_opts)
           return {
             url = opts.endpoint .. "/chat/completions",
             headers = {
               ["Accept"] = "application/json",
               ["Content-Type"] = "application/json",
+              ['x-api-key'] = 'ollama',
             },
             body = {
               model = opts.model,
-              messages = require("avante.providers").copilot.parse_message(code_opts),    -- you can make your own message, but this is very advanced
+              messages = require("avante.providers").copilot.parse_messages(code_opts),
               max_tokens = 2048,
               stream = true,
             },
           }
         end,
         parse_response_data = function(data_stream, event_state, opts)
-          require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+          require("avante.providers").copilot.parse_response(data_stream, event_state, opts)
         end,
       },
     },
+    behaviour = {
+      auto_suggestions = false, -- Experimental stage
+      auto_set_highlight_group = true,
+      auto_set_keymaps = true,
+      auto_apply_diff_after_generation = false,
+      support_paste_from_clipboard = true,
+    },
+    mappings = {
+      --- @class AvanteConflictMappings
+      diff = {
+        ours = 'co',
+        theirs = 'ct',
+        all_theirs = 'ca',
+        both = 'cb',
+        cursor = 'cc',
+        next = ']x',
+        prev = '[x',
+      },
+      suggestion = {
+        accept = '<M-l>',
+        next = '<M-]>',
+        prev = '<M-[>',
+        dismiss = '<C-]>',
+      },
+      jump = {
+        next = ']]',
+        prev = '[[',
+      },
+      submit = {
+        normal = '<CR>',
+        insert = '<C-s>',
+      },
+    },
+    hints = { enabled = true },
+    windows = {
+      ---@type "right" | "left" | "top" | "bottom"
+      position = 'right',   -- the position of the sidebar
+      wrap = true,          -- similar to vim.o.wrap
+      width = 30,           -- default % based on available width
+      sidebar_header = {
+        align = 'center', -- left, center, right for title
+        rounded = true,
+      },
+    },
+    highlights = {
+      ---@type AvanteConflictHighlights
+      diff = {
+        current = 'DiffText',
+        incoming = 'DiffAdd',
+      },
+    },
+    --- @class AvanteConflictUserConfig
+    diff = {
+      autojump = true,
+      ---@type string | fun(): any
+      list_opener = 'copen',
+    },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = "make",
+  build = "make BUILD_FROM_SOURCE=true",
   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
+    "nvim-treesitter/nvim-treesitter",
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    -- "zbirenbaum/copilot.lua",      -- for providers='copilot'
     {
       -- support for image pasting
       "HakonHarnes/img-clip.nvim",
@@ -54,8 +114,6 @@ return {
           drag_and_drop = {
             insert_mode = true,
           },
-          -- required for Windows users
-          use_absolute_path = true,
         },
       },
     },
@@ -66,6 +124,39 @@ return {
         file_types = { "markdown", "Avante" },
       },
       ft = { "markdown", "Avante" },
+    },
+  },
+  keys = {
+    {
+      "<leader>aa",
+      function()
+        require("avante.api").ask()
+      end,
+      desc = "avante: ask",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>ac",
+      function()
+        require("avante.api").ask()
+      end,
+      desc = "avante: chat",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>ar",
+      function()
+        require("avante.api").refresh()
+      end,
+      desc = "avante: refresh",
+    },
+    {
+      "<leader>ae",
+      function()
+        require("avante.api").edit()
+      end,
+      desc = "avante: edit",
+      mode = "v",
     },
   },
 }
