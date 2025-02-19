d0f5a99ed94389da91bc6ce65d87ae9f6a7b7bbe
John Saigle <john@asymmetric.re>
2025-02-19 11:07:17 -0500

    Move gitsigns and use it for blame operations

---
+++ lua/custom/plugins/gitsigns.lua
@@ -0,0 +1,18 @@
+return {
+  'lewis6991/gitsigns.nvim',
+  opts = {
+    signs = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
+    },
+  },
+  config = function()
+    local signs = require('gitsigns')
+    signs.setup({})
+    vim.keymap.set('n', '<leader>bl', ':Gitsigns blame<CR>', { desc = '[G]itsigns [B][L]ame' })
+    vim.keymap.set('n', '<leader>bn', ':Gitsigns blame_line<CR>', { desc = '[G]itsigns [B]lame Li[N]e' })
+  end
+}
--- lua/custom/plugins/vim-fugitive.lua
+++ lua/custom/plugins/vim-fugitive.lua
@@ -1,6 +1,5 @@
 return {
   'tpope/vim-fugitive',
   config = function()
-    vim.keymap.set('n', '<leader>gb', ':Git blame<CR>', { desc = '[G]it [B]lame' })
   end
 }
