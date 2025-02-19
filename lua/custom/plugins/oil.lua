7828182eeea89bd0cace97d2bcf4730e71193e69
John Saigle <john@asymmetric.re>
2025-02-19 10:53:55 -0500

    Add keymaps for oil

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,9 +1,15 @@
 return {
   'stevearc/oil.nvim',
   ---@module 'oil'
-  ---@type oil.SetupOpts
   opts = {},
   -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  config = function ()
+
+    local oil = require('oil')
+    oil.setup({})
+
+    -- Alias for `:Oil`
+    vim.keymap.set('n', '<leader>oi', '<cmd>Oil<cr>', {desc = 'Open Oil file explorer'})
+  end
 }
