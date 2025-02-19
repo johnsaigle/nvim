16ad0e5fd885844a56c5e6b30eb7b4cc072ab3ef
a <a>
2025-02-18 23:17:51 -0500

    Move trouble to its own file

--- init.lua
+++ init.lua
@@ -191,49 +191,6 @@ require('lazy').setup({
     },
     opts = {}
   },
-  { -- Trouble configuration (better experience with diagnostics)
-    "folke/trouble.nvim",
-    opts = {
-      win = {
-        wo = {
-          wrap = true,
-        },
-      },
-    },
-    cmd = "Trouble",
-    keys = {
-      {
-        "<leader>xx",
-        "<cmd>Trouble diagnostics toggle<cr>",
-        desc = "Diagnostics (Trouble)",
-      },
-      {
-        "<leader>xX",
-        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-        desc = "Buffer Diagnostics (Trouble)",
-      },
-      {
-        "<leader>cs",
-        "<cmd>Trouble symbols toggle focus=false<cr>",
-        desc = "Symbols (Trouble)",
-      },
-      {
-        "<leader>cl",
-        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-        desc = "LSP Definitions / references / ... (Trouble)",
-      },
-      {
-        "<leader>xL",
-        "<cmd>Trouble loclist toggle<cr>",
-        desc = "Location List (Trouble)",
-      },
-      {
-        "<leader>xQ",
-        "<cmd>Trouble qflist toggle<cr>",
-        desc = "Quickfix List (Trouble)",
-      },
-    },
-  },
   { import = 'custom.plugins' },
 }, {})
 
---
+++ lua/custom/plugins/trouble.lua
@@ -0,0 +1,35 @@
+return {
+  "folke/trouble.nvim",
+  opts = {
+    win = {
+      wo = {
+        wrap = true,
+      },
+    },
+  },
+  config = function()
+    require('trouble').setup({})
+
+    local trouble = require('trouble')
+
+    vim.keymap.set('n', '<leader>xx', "<cmd>Trouble diagnostics toggle<cr>")
+
+    vim.keymap.set('n', '<leader>xn', function()
+      trouble.next({ skip_groups = false, jump = true })
+    end)
+
+    vim.keymap.set('n', '<leader>xN', function()
+      trouble.next({ skip_groups = true, jump = true })
+    end)
+
+    vim.keymap.set('n', '<leader>xp', function()
+      trouble.prev({ skip_groups = false, jump = true })
+    end)
+
+    vim.keymap.set('n', '<leader>xP', function()
+      trouble.prev({ skip_groups = true, jump = true })
+    end)
+
+
+  end
+}
