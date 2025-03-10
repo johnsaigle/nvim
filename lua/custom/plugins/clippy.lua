c100bfb34e912986efb24b34fda1bc103e0bf949
John Saigle <john@asymmetric.re>
2025-03-10 10:38:57 -0400

    Add new clippy keymap

--- lua/custom/plugins/clippy.lua
+++ lua/custom/plugins/clippy.lua
@@ -1,6 +1,7 @@
 return {
   'johnsaigle/clippy.nvim',
   dependencies = { 'jose-elias-alvarez/null-ls.nvim' },
+  dir = '~/coding/clippy.nvim',
 
   config = function()
     local clippy = require('clippy')
@@ -9,7 +10,8 @@ return {
     vim.api.nvim_create_autocmd("FileType", {
       pattern = "rust",
       callback = function()
-        vim.keymap.set("n", "<leader>ll", clippy.clippy, { desc = "Run C[L]ippy [L]ints" })
+        vim.keymap.set("n", "<leader>ll", clippy.clippy, { desc = "c[L]ippy: Run [L]ints" })
+        vim.keymap.set("n", "<leader>lc", clippy.clear_diagnostics, { desc = "c[L]ippy: [C]lear diagnostics"})
       end
     })
   end
