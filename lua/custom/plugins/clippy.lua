ee7b8b6892078cdb6ce28e4169b1318e34e920f6
a <a>
2025-03-30 14:00:27 -0400

    stop using local clippy

--- lua/custom/plugins/clippy.lua
+++ lua/custom/plugins/clippy.lua
@@ -1,8 +1,6 @@
 return {
   'johnsaigle/clippy.nvim',
   dependencies = { 'jose-elias-alvarez/null-ls.nvim' },
-  dir = '~/coding/clippy.nvim',
-
   config = function()
     local clippy = require('clippy')
     clippy.setup({})
