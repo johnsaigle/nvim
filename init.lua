bf4691766726d46377d97966b2a69682c9107b15
John Saigle <john@asymmetric.re>
2025-06-20 16:46:42 -0400

    change binding for cmp scrolling to help with supermaven tab-complete

--- init.lua
+++ init.lua
@@ -367,7 +367,7 @@ cmp.setup {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-    ['<Tab>'] = cmp.mapping(function(fallback)
+    ['<C-n>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
@@ -377,7 +377,7 @@ cmp.setup {
       end
     end, { 'i', 's' }),
 
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
+    ['<C-p>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
@@ -390,6 +390,7 @@ cmp.setup {
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
+    { name = 'supermaven' },
     { name = 'treesitter' },
   },
 }
