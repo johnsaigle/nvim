2c8dcf44c806b091d4ae45b02a31703edc1fc9f7
John Saigle <john@asymmetric.re>
2025-11-02 13:08:41 -0500

    disable some buggy plugins

--- lua/custom/plugins/aderyn.lua
+++ lua/custom/plugins/aderyn.lua
@@ -3,8 +3,8 @@ return {
   'johnsaigle/aderyn.nvim',
   dependencies = { 'nvimtools/none-ls.nvim' },
   config = function()
-    local aderyn = require('aderyn-diagnostics')
-    aderyn.setup({})
+    -- local aderyn = require('aderyn-diagnostics')
+    -- aderyn.setup({})
 
   end
 }
