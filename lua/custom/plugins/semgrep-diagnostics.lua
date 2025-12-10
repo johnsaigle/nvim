ec8368aad98a3556f967cf1ad28420dc665bbff0
John Saigle <john@asymmetric.re>
2025-12-10 16:18:16 -0500

    re-reenable semgrep

--- lua/custom/plugins/semgrep-diagnostics.lua
+++ lua/custom/plugins/semgrep-diagnostics.lua
@@ -2,15 +2,15 @@ return {
   'johnsaigle/semgrep-diagnostics.nvim',
   dependencies = { 'nvimtools/none-ls.nvim' },
   config = function()
-    -- require('semgrep-diagnostics').setup({
-    --   enabled = true,
-    --   filetypes = { "sh", "bash", "lua", "rust", "go", "solidity" },
-    --   semgrep_config = {
-    --     "p/trailofbits",
-    --     "p/smart-contracts",
-    --     "~/coding/semgrep-rules",
-    --     "~/coding/semgrep-scary-strings",
-    --   },
-   -- })
+    require('semgrep-diagnostics').setup({
+      enabled = true,
+      filetypes = { "sh", "bash", "lua", "rust", "go", "solidity" },
+      semgrep_config = {
+        "p/trailofbits",
+        "p/smart-contracts",
+        "~/coding/semgrep-rules",
+        "~/coding/semgrep-scary-strings",
+      },
+   })
   end
 }
