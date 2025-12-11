95798890af25288124f45cba0a57e6f961b28ae6
John Saigle <john@asymmetric.re>
2025-12-11 12:41:40 -0500

    add revive.nvim

---
+++ lua/custom/plugins/revive.lua
@@ -0,0 +1,16 @@
+return {
+  "johnsaigle/revive.nvim",
+  dependencies = {
+    "johnsaigle/sast-nvim",
+    "nvimtools/none-ls.nvim",
+  },
+  config = function()
+    -- TODO: Rename module
+    require("revive-diagnostics").setup({
+      -- Optional: customize configuration
+      enabled = true,
+      filetypes = { "go" },
+      run_mode = "save", -- "save" or "change"
+    })
+  end,
+}
